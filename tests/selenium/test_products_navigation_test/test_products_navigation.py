a62e0e20aff4634a6ce44853a0902283a2f0517a
Chamseddine <khlifichamseddine@gmail.com>
2026-01-14 09:30:36 +0100

    update second test with hedless mode

--- tests/selenium/test_products_navigation_test/test_products_navigation.py
+++ tests/selenium/test_products_navigation_test/test_products_navigation.py
@@ -12,7 +12,7 @@ def test_products_navigation_and_verification():
     options = Options()
     options.binary_location = "/usr/bin/chromium-browser"
    
-    options.add_argument("--start-maximized") 
+    options.add_argument("----headless=new") 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
